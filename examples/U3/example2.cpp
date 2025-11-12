68c3a32f27141a97648a1d42edcac7e6dde54f1d
daniel-schwarzenbach <daniel.schwarzenbach@outlook.com>
2025-11-12 22:52:11 +0100

    added alternative access operator using chess coordinates

--- examples/U3/example2.cpp
+++ examples/U3/example2.cpp
@@ -23,10 +23,10 @@ struct ChessBoard {
 				
 			}
 		}
-		copy(board[0], "rnbqkbnr");
-		copy(board[1], "pppppppp");
-		copy(board[6], "PPPPPPPP");
-		copy(board[7], "RNBQKBNR");
+		copy(board[7], "rnbqkbnr");
+		copy(board[6], "pppppppp");
+		copy(board[1], "PPPPPPPP");
+		copy(board[0], "RNBQKBNR");
 	}
 	
 	// define an access operator
@@ -36,11 +36,18 @@ struct ChessBoard {
 		}
 		return board[x][y];
 	}
+
+	// alternate access operator using chess coordinates
+	char& operator[](const char* coords) {
+		int i1 = coords[0] - '1'; // row
+		int i2 = coords[1] - 'a'; // column
+		return board[i1][i2];
+	}
 };
 
 // define the output operator for the chess board
 std::ostream& operator<<(std::ostream& os, ChessBoard& cb) {
-	for (int i = 0; i < 8; ++i) {
+	for (int i = 7; i >= 0; --i) {
 		for (int j = 0; j < 8; ++j) {
 			os << cb(i, j);
 		}
@@ -56,6 +63,10 @@ int main() {
 	// set some pieces
 	chessBoard(2, 7) = 'X';
 	chessBoard(3, 3) = 'Y';
+	chessBoard["5a"] = 'Z';
+	char a = '6', b = 'b';
+	char coords[] = {a, b};
+	chessBoard[coords] = 'W';
 	// print the chess board
 	cout << chessBoard;
 }
