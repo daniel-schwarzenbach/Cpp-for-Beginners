ed29fcd85314839e8788a561b07eed461dae46af
daniel-schwarzenbach <daniel.schwarzenbach@outlook.com>
2025-11-13 11:08:20 +0100

    made logial or a seperate file

--- examples/U3/compile.sh
+++ examples/U3/compile.sh
@@ -1,7 +1,7 @@
 dir=$(dirname "$0")
 
 # compile all cpp files in this directory
-for file in ${dir}/*.cpp; do
+for file in ${dir}/example?.cpp; do
 		program=${file%.cpp}
 		g++-14 -g3 -DDebug -std=c++23 -Wall -Wextra -pedantic -O0 -o $program $file
 done
