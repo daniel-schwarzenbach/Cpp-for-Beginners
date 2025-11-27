f4455051ba081e2e61e9e95a42321c436513c305
daniel-schwarzenbach <daniel.schwarzenbach@outlook.com>
2025-11-27 05:18:56 +0100

    added comments

--- lesson2/lib/include/analysis.hpp
+++ lesson2/lib/include/analysis.hpp
@@ -3,9 +3,31 @@
 
 namespace math {
 
+// compute the derivative of a function at a point numerically
 flt derivative(Function<flt(flt)> const& func, flt point);
+
+/*
+returns the derivative function of a given function numerically
+```default
+d/dx f(x)
+```
+*/
 Function<flt(flt)> derivative(Function<flt(flt)> const& func);
+
+/*
+returns the integral of a function from a to b numerically
+```default
+ₐ∫ᵇ f(x) dx
+```
+*/
 flt integral(Function<flt(flt)> const& func, flt a, flt b);
+
+/*
+returns the integral function of a given function numerically
+```default
+₀∫ʸ f(x) dx
+```
+*/
 Function<flt(flt)> integral(Function<flt(flt)> const& func);
 
 
