7356affabb78833ae6c6502baa921a3c10dcd4c0
daniel-schwarzenbach <daniel.schwarzenbach@outlook.com>
2025-12-03 23:00:57 +0100

    added better comments

--- examples/U6/example2.cpp
+++ examples/U6/example2.cpp
@@ -6,22 +6,23 @@ using std::endl;
 using u64 = unsigned long long;
 using std::array;
 
-// maximum factorial precomputed
+// set maximum factorial to compute
 constexpr size_t MAX_FAC = 200;
 
-// create factorials at compile time
-constexpr array<u64, 200> make_factorials() {
-  array<u64, 200> facs{};
+// constexpr means this function can be evaluated at compile time
+constexpr array<u64, MAX_FAC> make_factorials() {
+  array<u64, MAX_FAC> facs{};
   facs[0] = 1;
-  for (u64 i = 1; i < 200; ++i) {
+  for (u64 i = 1; i < MAX_FAC; ++i) {
     facs[i] = i * facs[i - 1];
   }
   return facs;
 }
 
-// precompute factorials
+// Precompute factorials at compile time
 constexpr array<u64, MAX_FAC> factorials = make_factorials();
 
+// Lookup function for factorials
 u64 ultra_fast_fac(u64 n) {
   if (n >= (u64)MAX_FAC)
     return 0;
@@ -32,3 +33,4 @@ int main() {
   cout << ultra_fast_fac(6) << ", " << ultra_fast_fac(200) << endl;
   return 0;
 }
+
