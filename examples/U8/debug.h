055450eccba43c532c1ccbed23b2c1e075bc0a2e
daniel-schwarzenbach <daniel.schwarzenbach@outlook.com>
2025-12-18 11:22:57 +0100

    added check function

--- examples/U8/debug.h
+++ examples/U8/debug.h
@@ -8,13 +8,28 @@ using std::cout;
 using std::endl;
 
 #ifdef Debug
-#define info(x) cout << terminal::green << "𝑖 " << x << terminal::reset << endl;
-#define input(x) cout << terminal::blue << "> " << x << terminal::reset << endl;
-#define warning(x) cout << terminal::yellow << "! " << x << terminal::reset << endl;
-#define error(x) cout << terminal::red << "↯ " << x << terminal::reset << endl;
+// return important information
+#define info(x) cout << terminal::green << "𝑖 " << x << terminal::reset << endl
+// echo input commands
+#define input(x) cout << terminal::blue << "> " << x << terminal::reset << endl
+// warn about potential issues
+#define warning(x) cout << terminal::yellow << "! " << x << terminal::reset << endl
+// report errors
+#define error(x) cout << terminal::red << "↯ " << x << terminal::reset << endl
+// check a condition and report if it fails
+#define check(x)                   \
+  if (!(x)) {                      \
+    error("check failed: " << #x); \
+  }
 #else
+// return important information
 #define info(x)
+// echo input commands
 #define input(x)
+// warn about potential issues
 #define warning(x)
+// report errors
 #define error(x)
+// check a condition and report if it fails
+#define check(x)
 #endif
